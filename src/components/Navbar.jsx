c13040bf549326214f45488ef895dac2d2bdefb7
Hemazyn <hemazyn@gmail.com>
2023-08-09 05:40:33 +0100

    docx updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -28,7 +28,7 @@ const NavBar = () => {
      return (
           <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed z-50">
                <div>
-                    <h1 className="text-5xl font-signature ml-2">devEmma</h1>
+                    <h1 className="text-5xl font-signature ml-2 cursor-pointer">devEmma</h1>
                </div>
                <ul className="hidden md:flex">
                     {links.map(({ id, link }) => (
