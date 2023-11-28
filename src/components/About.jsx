456c064f94fe4b0ed887e8de4cb4d4d153c01b0f
Hemazyn <hemazyn@gmail.com>
2023-11-28 15:29:06 +0100

    updated

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const About = () => {
      return (
-          <div name="about" className="w-full bg-gradient-to-b from-gray-800 to-black text-white pt-[10vh] lg:pt-0">
+          <div name="about" className="w-full bg-gradient-to-b from-gray-800 to-black text-white pt-5 lg:pt-0">
                <div className="p-4 mx-auto flex flex-col justify-center w-[90%] h-full pt-0 lg:pt-20">
                     <div className="pb-4">
                          <p className="text-4xl font-bold inline border-b-4 border-gray-500">About</p>
